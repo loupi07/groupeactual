88f2144e2a414c9ddd6014fecd4a7fb82bf56896
loupi07 <pierre-louis.vaisman@epitech.eu>
2021-02-13 10:57:00 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Accueil from './components/Accueil';
+import Informations from './components/Informations';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Accueil} />
+          <Route exact path="/informations" component={Informations} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
